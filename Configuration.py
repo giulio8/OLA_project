ffcbdcfda7b17ea4b2157d94e20bea0fa02898fc
giulio8 <giuliosichili@gmail.com>
2024-05-19 15:54:25 +0200

    fixes

--- Configuration.py
+++ Configuration.py
@@ -14,6 +14,7 @@ N_ADVERTISERS = 10    # number of companies that compete for slot auctions, incl
 NUMBER_OF_ARMS = 10    # needed for the UCB algorithm
 MIN_PRICE = 0
 MAX_PRICE = 1
+COST = 0.1
 NUMBER_OF_PRICES = 100
 PRICES = np.linspace(MIN_PRICE, MAX_PRICE, NUMBER_OF_PRICES)
 BUDGET = 500
@@ -22,4 +23,4 @@ NUMBER_OF_SLOTS = 10
 lambda_fun_param = lambda s: 1/(s+(0.3))       # probability of the ad being seen given the position s ->  0.5 for first position, 0.33 for second, 0.25 for third, etc...
 LAMBDAS = np.array([lambda_fun_param(i) for i in range(1, NUMBER_OF_SLOTS+1)])    # array of lambda values (for each slot)
 
-DAYS = 100 # number of rounds of the pricing algorithm
+DAYS = 1000 # number of rounds of the pricing algorithm
